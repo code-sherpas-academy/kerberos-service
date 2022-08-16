0acf410c0b8c2965aaedae7ad77a05b48294b634
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-16 17:43:04 +0000

    Add get one permission feature

--- src/main/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/PermissionsController.kt
+++ src/main/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/PermissionsController.kt
@@ -1,11 +1,13 @@
 package rocks.codesherpas.academy.accelerate.backend.kerberosservice
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
@@ -14,6 +16,18 @@ class PermissionsController(private val permissionRepository: PermissionReposito
     @GetMapping("/permissions")
     fun getAll(): List<PermissionResourceWithId> {
         return permissionRepository.findAll().map { permission -> PermissionResourceWithId(permission.id, permission.description) }
+    }
+
+    @GetMapping("/permissions/{id}")
+    fun getOne(@PathVariable("id") id: String): PermissionResourceWithId {
+        val searchedPermission = permissionRepository.findById(id)
+
+        if (searchedPermission.isPresent) {
+            val permission = searchedPermission.get()
+            return PermissionResourceWithId(permission.id, permission.description)
+        } else {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "no permission with id $id")
+        }
     }
 
     @PostMapping("/permissions")
