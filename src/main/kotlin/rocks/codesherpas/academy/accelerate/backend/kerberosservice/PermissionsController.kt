516820d39d35113579a5071c291174f5ed71d258
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-16 18:01:44 +0200

    Rename controller as PermissionsController

--- src/main/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/PermissionsController.kt
+++ src/main/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/PermissionsController.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GetAllPermissionsController(private val permissionRepository: PermissionRepository) {
+class PermissionsController(private val permissionRepository: PermissionRepository) {
 
     @GetMapping("/permissions")
     fun getAll(): List<PermissionResource> {
