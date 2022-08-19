8e5b9d6c916ac55114b0379ea0630cc1d84d0ecc
Rui Sousa <ruisousa05@gmail.com>
2022-08-19 11:17:13 +0000

    Post permission container test

---
+++ src/test/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/PostPermissionCT.kt
@@ -0,0 +1,61 @@
+package rocks.codesherpas.academy.accelerate.backend.kerberosservice
+
+import com.google.gson.JsonParser
+import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostPermissionCT {
+
+    @LocalServerPort
+    val port:Int = 0
+    val expectedJson = """
+        {
+            "data": {
+                "id": null,
+                "description": "This is a post permission"
+            } 
+        }
+    """.trimIndent()
+
+    var requestBody:String = """
+        {
+            "description":"This is a post permission"
+        }
+    """.trimIndent()
+
+
+    @BeforeEach
+    fun setUp(){
+        RestAssured.port = port
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+    }
+
+
+    @Test
+    fun createPermission(){
+        Given {
+                contentType("application/json")
+                body(requestBody)
+        }When{
+                post("/permissions")
+
+        }Then{
+            statusCode(201)
+
+        }Extract{
+            val responseBody = JsonParser.parseString(body().asString())
+            val expectedBody = JsonParser.parseString(expectedJson)
+            //assertThat(responseBody).isEqualTo(expectedBody)
+        }
+    }
+
+}
