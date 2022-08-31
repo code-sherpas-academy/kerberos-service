cd552cc210b729d28ac7b4d5a5a9a0a8dcbace8a
Rui Sousa <ruisousa05@gmail.com>
2022-08-31 12:31:56 +0000

    Put role ct

---
+++ src/test/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/rolect/PutRoleCT.kt
@@ -0,0 +1,67 @@
+package rocks.codesherpas.academy.accelerate.backend.kerberosservice.rolect
+
+import com.google.gson.JsonParser
+import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.role.Role
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.role.RoleRepository
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PutRoleCT(
+    @Autowired val roleRepository: RoleRepository,
+    @LocalServerPort val port: Int
+) {
+
+    private val roleId = "123"
+    private val updatedDescription = "Updated description"
+    private val expectedJson = """
+        {
+            "id": "$roleId",
+            "description": "$updatedDescription",
+            "permissions": []
+        }
+    """.trimIndent()
+
+    @BeforeEach
+    fun setUp() {
+        RestAssured.port = port
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+
+        roleRepository.save(Role(roleId, "Initial description"))
+    }
+
+    @Test
+    fun updateRole() {
+        val updatedPermission = """
+            {
+                "description": "$updatedDescription",
+                "permissions": []
+            }
+        """.trimIndent()
+
+        Given {
+            pathParam("id", roleId)
+            contentType("application/json")
+            body(updatedPermission)
+        } When {
+            put("/roles/{id}")
+        } Then {
+            statusCode(200)
+            contentType("application/json")
+        } Extract {
+            val responseBody = JsonParser.parseString(body().asString())
+            val expectedBody = JsonParser.parseString(expectedJson)
+            Assertions.assertThat(responseBody).isEqualTo(expectedBody)
+        }
+    }
+}
