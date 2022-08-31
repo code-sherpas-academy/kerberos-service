30089178fcf3a58961f14c6b570fa2724c5db354
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-31 11:03:12 +0000

    Get single role container test

---
+++ src/test/kotlin/rocks/codesherpas/academy/accelerate/backend/kerberosservice/rolect/GetSingleRoleCT.kt
@@ -0,0 +1,72 @@
+package rocks.codesherpas.academy.accelerate.backend.kerberosservice.rolect
+
+import com.google.gson.JsonParser
+import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.permission.Permission
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.permission.PermissionRepository
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.role.Role
+import rocks.codesherpas.academy.accelerate.backend.kerberosservice.role.RoleRepository
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetSingleRoleCT(
+    @Autowired val roleRepository: RoleRepository,
+    @Autowired val permissionRepository: PermissionRepository,
+    @LocalServerPort val port: Int
+) {
+
+    private val roleId = "123"
+    private val roleDescription = "This is a role"
+    private val permissionId = "456"
+    private val permissionDescription = "This is a description"
+    private val expectedJson = """
+        {
+            "id": "$roleId",
+            "description": "$roleDescription",
+            "permissions": [
+                {
+                    "id": "$permissionId",
+                    "description": "$permissionDescription"
+                }
+            ]
+        }
+    """.trimIndent()
+
+    @BeforeEach
+    fun setUp() {
+        RestAssured.port = port
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+
+        val role = Role(roleId, roleDescription)
+        val permission = Permission(permissionId, permissionDescription)
+        permissionRepository.save(permission)
+        role.permissions.add(permission)
+
+        roleRepository.save(role)
+    }
+
+    @Test
+    fun getSingleRole() {
+        Given {
+            pathParam("id", roleId)
+        } When {
+            get("/roles/{id}")
+        } Then {
+            statusCode(200)
+            contentType("application/json")
+        } Extract {
+            val responseBody = JsonParser.parseString(body().asString())
+            val expectedBody = JsonParser.parseString(expectedJson)
+            Assertions.assertThat(responseBody).isEqualTo(expectedBody)
+        }
+    }
+}
